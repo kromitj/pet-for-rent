a6fb2f238b0206a3325d0c2fc3ad98fe89cf09e1
Mitch Kroska <kromitj@gmail.com>
2017-10-23 19:42:36 -0500

    Init Admin model

---
+++ db/migrate/20171024002906_create_admins.rb
@@ -0,0 +1,13 @@
+class CreateAdmins < ActiveRecord::Migration[5.0]
+  def change
+    create_table :admins do |t|
+    	t.string :first_name
+    	t.string :last_name
+    	t.string :email
+    	t.string :password_digest
+    	t.string :password_digest_2
+    	
+      t.timestamps
+    end
+  end
+end
